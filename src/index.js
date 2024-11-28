365dd7cd57ff1f856a639390395a082c2ca5ed32
KR-ISCOD3 <krisjenxz@example.com>
2024-11-28 11:43:14 +0700

    update-cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {config} = require("dotenv");
+const cors = require('cors');
 const connection = require('./config/db');
 const router = require('./routes/studentroute');
 config()
@@ -11,6 +12,7 @@ const host = process.env.HOST || "localhost";
 connection()
 
 app.use(express.json())
+app.use(cors());
 app.use('/api',router)
 
 app.listen(port,()=>{
