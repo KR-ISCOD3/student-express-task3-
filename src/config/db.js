ad46f25aadbf4a2f455e914a37c90b8d7c398d0a
KR-ISCOD3 <krisjenxz@example.com>
2024-11-28 12:00:39 +0700

    connection-update

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,23 @@
-const {config} = require("dotenv")
-const mongoose = require("mongoose")
+const { config } = require("dotenv");
+const mongoose = require("mongoose");
 
-config()
+config();
 
-const url = process.env.MONGO_URL
+const url = process.env.MONGO_URL;
 
 const connection = async () => {
-    try{
-        mongoose.connect(url)
-        .then(()=>console.log("Connected"))
-        .catch(err => console.error(err));
-    }catch(e){
-        console.error("Error:",e);
-    }
-}
+  try {
+    mongoose
+      .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000,
+      })
+      .then(() => console.log("Connected"))
+      .catch((err) => console.error(err));
+  } catch (e) {
+    console.error("Error:", e);
+  }
+};
 
-module.exports = connection
+module.exports = connection;
